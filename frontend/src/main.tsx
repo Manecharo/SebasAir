86f3a294162c31fe01e5d841fdf33387e59ca677
Manecharo <manuel_echavarria@ymail.com>
2025-03-09 20:19:21 +0800

    Fix TypeScript configuration and interface issues

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import './i18n/i18n.ts'
+import './i18n/i18n'
 import 'leaflet/dist/leaflet.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
